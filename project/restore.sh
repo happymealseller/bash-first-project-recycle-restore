0dd038a3604dc47c0053ce83e24ca76ae1b2f71a
Ben <happymealseller@gmail.com>
2022-12-22 21:06:09 +0800

    Updated phase 2, not fully working

--- project/restore.sh
+++ project/restore.sh
@@ -1,28 +1,43 @@
-#!/bin/bash
+#! /bin/bash
 
 # validate we have at least one argument
-if [ $# -eq 0 ]; then     
+if [ $# -eq 0 ]; then
     echo "No filename provided -- usage: bash restore [options] [file...] –- operation aborted" 
-    exit 1   
+    exit 1
 fi
 
 # validate if we can find the file in recyclebin
-if [ find /$HOME/recyclebin -name $1] ; then    
+if [ ! -f /$HOME/recyclebin/$1 ] ; then
     echo "File does not exist –- operation aborted" 
-    exit 1   
+    exit 1
 fi
 
-# Check if file is going to be overwritten and prompt for input.
-if [ grep -w $1 n .restore.info ]; then 
+# validate if file already exists in pwd .
+if [ -f ./$1 ]; then
     echo "Do you want to overwrite? y/n"
     read input
+    # prompt for input to overwrite file
+    if [ -nocase $input = y* ]; then
+      :
+    else
+      exit 1
+    fi
 fi
 
-# Restore and edit log.
-if [ input === lower(Y*) ]; then
-  mv fileName $(grep path from restore info)
-  find out how to remove line from file 
-  exit 0
-fi
+# get original file name
+originalName=$(grep $1 /$HOME/.restore.info | cut -d: -f1)
 
-exit 1
+# get absolute path
+path=$(grep $1 /$HOME/.restore.info | cut -d: -f2)
+
+# move file back to orignial place rename it
+mv $1 $path/$originalName
+
+# rewrite file without selected log (test last)
+# grep -v $1 /$HOME/.restore.info > /$HOME/.restore.info
+
+# output success message
+echo "Success! -- $originalName restored"
+
+# quit
+exit 0
