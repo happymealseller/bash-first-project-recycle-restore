2ba587ef926ebefc600f7578cbe406078714989f
Ben <happymealseller@gmail.com>
2022-12-22 23:16:08 +0800

    Completed Phase 1 and 2

--- project/restore.sh
+++ project/restore.sh
@@ -12,29 +12,30 @@ if [ ! -f /$HOME/recyclebin/$1 ] ; then
     exit 1
 fi
 
-# validate if file already exists in pwd .
-if [ -f ./$1 ]; then
-    echo "Do you want to overwrite? y/n"
+# validate if file already exists in pwd 
+if [ -f ./$(echo $1 | cut -d"_" -f1) ]; then
+    echo "File with similar name exists -- do you want to overwrite? y/n"
     read input
     # prompt for input to overwrite file
-    if [ -nocase $input = y* ]; then
+    if [[ $input =~ ^[Yy] ]]; then
       :
     else
+      echo "Operation aborted" 
       exit 1
     fi
 fi
 
 # get original file name
-originalName=$(grep $1 /$HOME/.restore.info | cut -d: -f1)
+originalName=$(grep $1 /$HOME/.restore.info | cut -d":" -f1 | cut -d"_" -f1)
 
 # get absolute path
-path=$(grep $1 /$HOME/.restore.info | cut -d: -f2)
+path=$(grep $1 /$HOME/.restore.info | cut -d":" -f2)
 
 # move file back to orignial place rename it
 mv /$HOME/recyclebin/$1 $path/$originalName
 
-# rewrite file without selected log (test last)
-# grep -v $1 /$HOME/.restore.info > /$HOME/.restore.info
+# select all except current entry and overwrite log file without selected log
+sed -i "/$1/d" /$HOME/.restore.info
 
 # output success message
 echo "Success! -- $originalName restored"
